c2cbb8cf28306c6d762db9bdd3a34b357955899f
Rodrigo Schneider <rodrigoschneider94@gmail.com>
2018-01-13 17:59:28 +0100

    Added validation to user message input

--- tools.js
+++ tools.js
@@ -14,9 +14,10 @@ module.exports = {
     });
   },
   checkUserResponse: function(userResponse, productsList) {
+    if(userResponse === '') var error = "Sorry, you should type something.";
     var matchedProduct = productsList.find(function(product) {
       return product.name === userResponse;
     });
-    return "The subscription price of the asked product is: " +  matchedProduct.subscriptionPrice;
+    return matchedProduct && !error ? "The subscription price of the asked product is: " + matchedProduct.subscriptionPrice : !error ? "Sorry, no product was found with this criteria" : error;
   }
 };
